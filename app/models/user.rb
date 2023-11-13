5352fbb304c3a3c0327ed1240666cc6db00f6c41
Franco Pignatelli <francopignatelli15@gmail.com>
2023-11-13 14:06:06 -0300

    Agregadas las vistas de devise, generada pagina home y seteada como root, cambiados componentes new.html.erb de sessions (Log in): Agregado Link para volver al home; y de registrations (Sign Up): Agregados campos para la creacion de un usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
 end
