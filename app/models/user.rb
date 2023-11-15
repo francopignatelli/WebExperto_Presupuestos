6d2420ebff61fc3f46ef43d133a7a6f087b90714
Franco Pignatelli <francopignatelli15@gmail.com>
2023-11-15 14:13:01 -0300

    -Cambiado application_controller para que se pueda editar un user. - Cambiado user model pra que los campos agregados no sean verificados. - Cambiado new y edit para sartisfacer requerimientos del ticket Arreglar modelo user. - Agregada una traduccion, error de contraseña.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true
-  validates :last_name, presence: true
-  validates :phone, presence: true
-  validates :address, presence: true
 end
