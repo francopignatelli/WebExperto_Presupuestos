d302cfdfbb69b1ae0f9d6d4659ce5e4ee23c4d7a
TomasP <tomasperesin@gmail.com>
2023-11-16 15:27:10 -0300

    deje mas lindo el home y se puede navegar por las pestañas importantes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
 end
