78e5b073517a79601a038721c766d044ba469a98
TomasP <tomasperesin@gmail.com>
2023-11-23 16:00:09 -0300

    Se comenzo el JWT pero fracaso, las bases estan queda resolver el problema. Por lo demas la api funciona

--- app/controllers/api/V1/budgets_controller.rb
+++ app/controllers/api/V1/budgets_controller.rb
@@ -1,51 +1,29 @@
 module Api
     module V1
       class BudgetsController < ApplicationController
-        before_action :set_budget, only: %i[show update destroy]
-        before_action :authenticate_request!
+        before_action :set_budget, only: %i[show]
+        # before_action :authenticate_request!
 
       # GET /api/v1/budgets
       def index
-        budgets = current_user.budgets
-        render json: { budgets: budgets }
+        @budgets = Budget.all.includes(:lineitems)
+
+
+        render json: { budgets: @budgets.as_json(include: :lineitems)  }
       end
 
-      # GET /api/v1/budgets/1
+      # GET /api/v1/budgets/*params
       def show
-        render json: { budget: @budget }
-      end
+        debugger
+        @budgets = Budget.all.includes(:lineitems)
 
-      # POST /api/v1/budgets
-      def create
-        @budget = Budget.new(budget_params)
-        @budget.user = current_user
+        # Filtrar por nombre si el parámetro :name está presente
+        @budgets = @budgets.where(name: params[:name]) if params[:name].present?
 
-        if @budget.save
-          render json: { budget: @budget }, status: :created
-        else
-          render json: { errors: @budget.errors }, status: :unprocessable_entity
-        end
-      end
+        # Filtrar por la descripcion si el parámetro :description está presente
+        @budgets = @budgets.where(description: params[:description]) if params[:description].present?
 
-      # PATCH/PUT /api/v1/budgets/1
-      def update
-        if @budget.update(budget_params)
-          render json: { budget: @budget }, status: :ok
-        else
-          render json: { errors: @budget.errors }, status: :unprocessable_entity
-        end
-      end
-
-      # DELETE /api/v1/budgets/1
-      def destroy
-        if @budget.lineitems.empty?
-          @budget.destroy
-          head :no_content
-        else
-          @budget.lineitems.each(&:destroy)
-          @budget.destroy
-          head :no_content
-        end
+        render json: { budgets: @budgets.as_json(include: :lineitems)  }
       end
 
       private
@@ -61,7 +39,6 @@ module Api
       def authenticate_request!
         if request.headers['Authorization'].present?
             token = request.headers['Authorization'].gsub('Bearer ', '')
-            debugger
             decoded_token = JsonWebToken.decode(token)
 
             unless decoded_token
