fac51f4cd7c92440ebca485c6a191a8dbb64fef8
Octavio <om.alvarez21@gmail.com>
2023-11-30 13:49:21 -0300

    Agregando iniciar_sesion

--- db/schema.rb
+++ db/schema.rb
@@ -42,15 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_165240) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "api_users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "budgets", force: :cascade do |t|
     t.text "description"
     t.string "name"
